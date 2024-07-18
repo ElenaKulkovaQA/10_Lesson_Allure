ba630a51eb852038d13c3a21503fe7b0cf9d1a3a
Elena <elena.regiionh@gmail.com>
2024-07-18 18:23:15 +0300

    delete base url

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -7,7 +7,6 @@ public class TestBase {
     @BeforeAll
     static void setUpBefore() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://github.com/";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = false;
     }
